424cf72838cee3db18a1aeb73fe31836951f8b87
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-08-26 16:09:04 +0600

    added navbar image and hero image and also added the components folder and Navbar.jsx

---
+++ src/components/custom/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="py-1">
+      <div className="baseContainer"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
