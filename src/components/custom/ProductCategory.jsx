07c786c8a8954447af58b6358e3609d044ada505
pro-majidul <majidul123tub@gmail.com>
2025-08-27 15:31:39 +0600

    all design added

--- src/components/custom/ProductCategory.jsx
+++ src/components/custom/ProductCategory.jsx
@@ -1,52 +1,52 @@
-import Link from 'next/link';
-import React from 'react';
-import EmblaCarousel from '../common/EmblaCarousel';
+import Link from "next/link";
+import React from "react";
+import EmblaCarousel from "../common/EmblaCarousel";
 
 const ProductCategory = () => {
   const products = [
     {
-      name: 'camra',
-      src: '/images/productcategory/camra.png',
+      name: "camra",
+      src: "/images/productcategory/camra.png",
       items: 8,
     },
     {
-      name: 'car',
-      src: '/images/productcategory/car.png',
+      name: "car",
+      src: "/images/productcategory/car.png",
       items: 7,
     },
     {
-      name: 'chair',
-      src: '/images/productcategory/chair.png',
+      name: "chair",
+      src: "/images/productcategory/chair.png",
       items: 3,
     },
     {
-      name: 'console',
-      src: '/images/productcategory/console.png',
+      name: "console",
+      src: "/images/productcategory/console.png",
       items: 7,
     },
     {
-      name: 'console2',
-      src: '/images/productcategory/console2.png',
+      name: "console2",
+      src: "/images/productcategory/console2.png",
       items: 9,
     },
     {
-      name: 'graphic_card',
-      src: '/images/productcategory/graphic_card.png',
+      name: "graphic_card",
+      src: "/images/productcategory/graphic_card.png",
       items: 3,
     },
     {
-      name: 'keyboard',
-      src: '/images/productcategory/keyboard.png',
+      name: "keyboard",
+      src: "/images/productcategory/keyboard.png",
       items: 4,
     },
     {
-      name: 'laptop',
-      src: '/images/productcategory/laptpo.png',
+      name: "laptop",
+      src: "/images/productcategory/laptpo.png",
       items: 7,
     },
     {
-      name: 'monitor',
-      src: '/images/productcategory/monitor.png',
+      name: "monitor",
+      src: "/images/productcategory/monitor.png",
       items: 10,
     },
   ];
@@ -54,9 +54,9 @@ const ProductCategory = () => {
   return (
     <section className="py-5">
       <div className="baseContainer flex flex-col gap-1 md:gap-2">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-2">
-          <div className="flex flex-col">
-            <h2 className="text-xl md:text-2xl font-bold">
+        <div className="flex items-center justify-between">
+          <div className="flex w-3/2 flex-col">
+            <h2 className="text-xl md:text-2xl font-bold ">
               Popular Categories
             </h2>
             <p className="font-light text-sm">
@@ -64,8 +64,8 @@ const ProductCategory = () => {
             </p>
           </div>
           <Link
-            href={'#'}
-            className="underline text-blue-500 hover:text-blue-600"
+            href={"#"}
+            className=" text-sm md:text-lg w-1/2 text-blue-500 hover:text-blue-600 text-end"
           >
             View All
           </Link>
